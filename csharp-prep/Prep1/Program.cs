d8ec2832dba752546f71b121268ce7317dba715a
Bryan C. Stauch <sta15060@byui.edu>
2024-04-26 22:02:53 -0500

    Completed the Prep1 Assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your first name? ");
+        string fName = Console.ReadLine();
+        
+        Console.Write("What is your first last name? ");
+        string lName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
