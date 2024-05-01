cb60e6928d8ee644414ce37bd10ce98c4dab7416
Bryan C. Stauch <sta15060@byui.edu>
2024-04-30 22:57:57 -0500

    Completed Prep3 with the stretch.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 
 class Program
 {
@@ -6,33 +7,46 @@ class Program
     {
         int magicNumber;
         int guessedNumber;   
+        int loopCounter = 0;
+        string response;
+
+            Console.Clear(); //clears the console for a fresher look
+            
+            Console.WriteLine("Hello Prep3 World!");
+            
+            //Console.Write("What is the magic number? "); //Randomizing the magicNumber now.
+            //magicNumber = int.Parse(Console.ReadLine()); //Randomizing the magicNumber now.            
+
+
         do
         {
-            Console.Clear(); //clears the console for a fresher look
-            Console.WriteLine("Hello Prep3 World!");
-            Console.Write("What is the magic number? ");
-            magicNumber = int.Parse(Console.ReadLine());
-            
-            Console.Write("What is guess? ");
+            Random randomGenerator = new Random();
+            magicNumber = randomGenerator.Next(1,100);          
+
+            Console.Write("Pick a number between 1 and 100: ");
             guessedNumber = int.Parse(Console.ReadLine());
 
-            if (guessedNumber > magicNumber)
+            do
             {
-                Console.Clear(); //clears the console for a fresher look
-                Console.WriteLine("Lower");
-            }
+                loopCounter = ++loopCounter;
+                if (guessedNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
 
-            else if (guessedNumber < magicNumber)
-            {
-                Console.Clear(); //clears the console for a fresher look
-                Console.WriteLine("Higher");
-            }
+                else if (guessedNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");   
+                }
 
-            else if (guessedNumber == magicNumber)
-            {
-                Console.Clear(); //clears the console for a fresher look
-                Console.WriteLine("Congratulations, you are the winner!");
-            }
-        } while (magicNumber != guessedNumber);
+                Console.Write("What is your guess? ");
+                guessedNumber = int.Parse(Console.ReadLine());
+
+            } while (magicNumber != guessedNumber);
+            Console.Clear(); //clears the console for a fresher look
+            Console.WriteLine($"You guessed the Magic Number in {loopCounter + 1} tries.");
+            Console.WriteLine("Would you like to play again? (yes/no)");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
