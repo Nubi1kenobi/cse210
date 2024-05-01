4a42c016ed023780dbec63d2f4a91a19af296bb1
Bryan C. Stauch <sta15060@byui.edu>
2024-04-30 22:25:25 -0500

    Prep3 is mostly done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,7 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-        Console.Read("Blah Blah _____");
+        int magicNumber;
+        int guessedNumber;   
+        do
+        {
+            Console.Clear(); //clears the console for a fresher look
+            Console.WriteLine("Hello Prep3 World!");
+            Console.Write("What is the magic number? ");
+            magicNumber = int.Parse(Console.ReadLine());
+            
+            Console.Write("What is guess? ");
+            guessedNumber = int.Parse(Console.ReadLine());
+
+            if (guessedNumber > magicNumber)
+            {
+                Console.Clear(); //clears the console for a fresher look
+                Console.WriteLine("Lower");
+            }
+
+            else if (guessedNumber < magicNumber)
+            {
+                Console.Clear(); //clears the console for a fresher look
+                Console.WriteLine("Higher");
+            }
+
+            else if (guessedNumber == magicNumber)
+            {
+                Console.Clear(); //clears the console for a fresher look
+                Console.WriteLine("Congratulations, you are the winner!");
+            }
+        } while (magicNumber != guessedNumber);
     }
 }
