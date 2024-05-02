8cd4a4de453e122602a7d437896f4d8689d8d0eb
Bryan C. Stauch <sta15060@byui.edu>
2024-05-01 21:44:34 -0500

    Working Through Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        List<int> integerList = new List<int>();
+        int enteredNumber;
+
+        Console.Clear(); //clears the console for a fresher look
         Console.WriteLine("Hello Prep4 World!");
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        Console.Write("Enter number: ");
+        enteredNumber = int.Parse(Console.ReadLine());
+
+        do while (enteredNumber != 0)
+        {
+            integerList.Add(enteredNumber);
+            Console.Write("Enter number: ");
+            enteredNumber = int.Parse(Console.ReadLine());
+        }
     }
 }
