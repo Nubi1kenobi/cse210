d876c30bdde54d9aa7dacbd470a4c9c707d9d4a9
Bryan C. Stauch <sta15060@byui.edu>
2024-05-02 12:42:43 -0500

    completed prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear(); //clears the console for a fresher look
         Console.WriteLine("Hello Prep5 World!");
+        string userName;
+        int favNumber;
+        int squaredNumber;
+
+        DisplayWelcome();
+        userName = PromptUserName();
+        favNumber = PromptUserNumber();
+        squaredNumber = SquareNumber(favNumber);
+        DisplayResult(userName, squaredNumber);
+
+
+        //Static Functions
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine(); 
+            return name;
+        }
+        
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int aNumber = int.Parse(Console.ReadLine()); 
+            return aNumber;
+        }
+        
+        static int SquareNumber(int a)
+        {
+            int square = (a * a);
+            return square;
+        }
+        
+        static void DisplayResult(string a, int b)
+        {
+            Console.WriteLine($"{a}, the square of your number is {b}");
+        }
+ 
     }
 }
