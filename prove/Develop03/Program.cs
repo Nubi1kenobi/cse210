38f2a9b2409ded50f49c537f6acbd9493cf3f44b
Bryan C. Stauch <sta15060@byui.edu>
2024-06-21 11:39:30 -0500

    project complete

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,65 +2,64 @@ using System;
 using System.Security.Cryptography.X509Certificates;
 
 class Program
-{
-    static void Main(string[] args)
-        {
-            bool inputValidation1 = false;
-            bool inputValidation2 = false;
-            string userInput2 = "";
-            do
-                {
-                    //string mainScripture = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";       
-                    string mainScripture = "In all thy ways acknowledge him, and he shall direct thy paths.";       
-                    Reference newReference = new Reference("Proverbs", "3", "5", "6");   
-                    Scripture newScripture = new Scripture(mainScripture);
-                    int loopCount = newScripture.GetWordCount();   
-                    Thread.Sleep(250); //0.25s delay
-                    Console.Clear();
-                    Console.WriteLine($"{newReference.GetReference()} {newScripture.GetScripture()}\n\nPress enter to continue or type 'quit' to finish:");
-                        do
-                            {
-                                loopCount--;                    
-                                userInput1 = Console.ReadLine();
-                                inputValidation1 = handleInput1(userInput1);
-                                if (inputValidation1 == true || loopCount < 0) { break; }
-                                Thread.Sleep(250); //0.25s delay
-                                //Console.Clear();
-                                Console.WriteLine($"{newReference.GetReference()} {newScripture.GetUpdatedScripture()}\n\nPress enter to continue or type 'quit' to finish:");
-                                DebugUtility.Debug(inputValidation1);
-                            }   while (!inputValidation1);
-                            DebugUtility.Debug();
-                    Console.WriteLine("Would you like to try again? 'y' for yes.> ");
-                    userInput2 = Console.ReadLine();
-                    inputValidation1 = handleInput2(userInput2);
-                } while (!inputValidation2);
-
-                static bool handleInput1(string userInput)
-                        {
-                            string _userInput = userInput;
-                            if (_userInput == "quit") { Quit(); }
-                            return false;
-                        }
-                    
-                static bool handleInput2(string userInput)
+    {
+        static void Main(string[] args)
+            {
+                Start();
+            }
+                static void Start()
                     {
-                        string _userInput2 = userInput;
-                        if (_userInput2 != "y") { return false; }
-                        else { Quit(); }
-                        return false;
+                        bool scriptureLoopInputValidation = false;
+                        string userScriptureLoopInput = "";
+                        int unHiddenWordsRemainging = 0;
+                        string mainScripture = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";       
+                        Reference newReference = new Reference("Proverbs", "3", "5", "6");   
+                        Scripture newScripture = new Scripture(mainScripture);
+                        int loopCount = newScripture.GetWordCount();   
+                        Thread.Sleep(250); //0.25s delay
+                        Console.Clear();
+                        Console.WriteLine($"{newReference.GetReference()} {newScripture.GetScripture()}\n\nPress enter to continue or type 'quit' to finish:");
+                        Console.WriteLine($"There are {newScripture.GetWordCount()} words in this scripture.");
+                            do
+                                {
+                                    loopCount = newScripture.GetWordCount() - newScripture.GetFlaggedWordCount();
+                                    //DebugUtility.Debug(loopCount);
+                                    userScriptureLoopInput = Console.ReadLine();
+                                    scriptureLoopInputValidation = handleScriptureLoopInput(userScriptureLoopInput);
+                                    if (scriptureLoopInputValidation || loopCount < 1) { Quit(mainScripture); }
+                                    Thread.Sleep(250); //0.25s delay
+                                    Console.Clear();
+                                    Console.WriteLine($"{newReference.GetReference()} {newScripture.GetUpdatedScripture()}\n\nPress enter to continue or type 'quit' to finish:");
+                                    unHiddenWordsRemainging = newScripture.GetWordCount() - newScripture.GetFlaggedWordCount();
+                                    Console.WriteLine($"There are {unHiddenWordsRemainging} unhidden word(s) remaining.");
+                                    //DebugUtility.Debug(userScriptureLoopInput);                         
+                                }   while (!scriptureLoopInputValidation );
+                
+                            bool handleScriptureLoopInput(string userInput)
+                                {
+                                    string _userInput = userInput;
+                                    if (_userInput == "quit") { Quit(mainScripture); }
+                                    return false;
+                                }
                     }
-
-                static void Quit()
+                    
+                static void Quit(string scripture)
                     {
                         Console.Clear();
-                        Console.WriteLine("Goodbye, Au revoir, Auf Wiedersehen, Yasou, L'hitraot, Namaste, Viszlát!, Vertu Saeill!, Atsiprasau, Zài jiàn, Namaste, Ha det bra, Khodaa haafez, Żegnaj, Adeus , Adiós, Chao, Dasvidaniya, Poka, Bài bài, Zàijiàn, Arrivederci, Ciao, Sayonara, Ja nee, Farvel, Kwaheri, Baadaye");
-                        for (int i = 0; i < 10; i++)
-                            {
-                                Console.WriteLine("");
-                            }
-                        System.Environment.Exit(0);
+                        Console.WriteLine("If you would like to try again, press 'y'. Press any other key and enter to quit.");
+                        string _userInput2 = Console.ReadLine();
+                        if (_userInput2 == "y") { Start(); }
+                        else    {
+                                    Console.Clear();
+                                    Console.WriteLine("Goodbye, Au revoir, Auf Wiedersehen, Yasou, L'hitraot, Namaste, Viszlát!, Vertu Saeill!, Atsiprasau, Zài jiàn, Namaste, Ha det bra, Khodaa haafez, Żegnaj, Adeus , Adiós, Chao, Dasvidaniya, Poka, Bài bài, Zàijiàn, Arrivederci, Ciao, Sayonara, Ja nee, Farvel, Kwaheri, Baadaye");
+                                    for (int i = 0; i < 10; i++)
+                                        {
+                                            Console.WriteLine("");
+                                        }
+                                    System.Environment.Exit(0);
+                                }
                     }
-            }
-}
+            
+    }
 
 
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,82 +1,89 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.Serialization;
 public class Scripture
-{
-//object variables***********************************
-    private string _scripture;
-    private string[] _scriptureWords;
-    private List<Word> _scriptureWordList = new List<Word>();
-
-
-//contructors**************************************
-public Scripture(string scripture)
-{
-    _scripture = scripture;
-    _scriptureWords = _scripture.Split(" ");
-}    
-
-//methods/Getters/Setters**************************************
-    public string GetScripture() 
-        {
-            return _scripture; 
-        }
-   
-    // private string[] GetScriptureWords()
-    // {
-    //     return _scriptureWords;
-    // }
-
-    // private void SetScripture(string scripture) 
-    //     { 
-    //         _scripture = scripture;
-    //     }
-    
-    public int GetWordCount()
     {
-        //Console.WriteLine($"There are {_scriptureWords.Length} words stored in the array");
-        return _scriptureWords.Length;
-    }
+    //object variables***********************************
+        private string _scripture;
+        private string[] _scriptureWords;
+        private int _flaggedWords = 0;
+        private List<Word> _scriptureWordList = new List<Word>();
+        
 
-    public string GetUpdatedScripture()
+
+    //contructors**************************************
+    public Scripture(string scripture)
         {
-            foreach (string wordText in _scriptureWords)
-                {
-                    Word word = new Word();
-                    _scriptureWordList.Add(word);
-                }
-    
-            // _scriptureWordList[6].SetHidden();
-            _scriptureWordList[ElRandomo()].SetHidden();
+            _scripture = scripture;
+            _scriptureWords = _scripture.Split(" ");
+        }    
 
-            for (int i = 0; i < _scriptureWordList.Count; i++)
+    //methods/Getters/Setters**************************************
+        public string GetScripture() 
             {
-                if (_scriptureWordList[i].GetStatus())
-                {
-                    _scriptureWords[i] = "".PadLeft(_scriptureWords[i].Length, '_');
-                }
+                return _scripture; 
             }
-            
-            _scripture = string.Join(" ", _scriptureWords);
-            return _scripture;
-        }
- 
-    private int ElRandomo()
-    {
-        bool isValid = false;
-        int _theRandomNumber;
-
-        var _randomizer = new Random();
-        do                                                  
+        
+        public int GetWordCount()
             {
-                _theRandomNumber = _randomizer.Next(0,GetWordCount() - 1);
-                if (_scriptureWordList[_theRandomNumber].GetStatus() == true) // checks for hidden state
-                    {   
-                        _theRandomNumber = _randomizer.Next(1,_scriptureWordList.Count);
+                return _scriptureWords.Length;
+            }
+
+        public int GetFlaggedWordCount()
+            {
+                return _flaggedWords;
+            }
+
+        public string GetUpdatedScripture()
+            {
+                foreach (string wordText in _scriptureWords)
+                    {
+                        Word word = new Word();
+                        _scriptureWordList.Add(word);
                     }
-                    else isValid = true;
-            }
-        while (!isValid);
-        return _theRandomNumber;
-    }
+        
+                HideMultipleWords();
 
-}
+                for (int i = 0; i < _scriptureWordList.Count; i++)
+                    {
+                        if (_scriptureWordList[i].GetStatus())
+                            {
+                                _scriptureWords[i] = "".PadLeft(_scriptureWords[i].Length, '_');
+                            }
+                    }
+                
+                _scripture = string.Join(" ", _scriptureWords);
+                return _scripture;
+            }
+    
+        private void HideMultipleWords()
+            {
+                int _randomNumber;
+                var rando = new Random();
+                int _rCount = 4;
+                int diff = _scriptureWords.Count()-_flaggedWords;
+                if (diff < 2) {_rCount = 1;}
+                _randomNumber = rando.Next(1,_rCount);
+                for (int i = _randomNumber; i > 0; i--) { _scriptureWordList[ElRandomo()].SetHidden(); }
+                _flaggedWords = _flaggedWords + _randomNumber;
+
+            }
+        private int ElRandomo()
+            {
+                bool _isValid = false;
+                int _theRandomNumber;
+                var _randomizer = new Random();
+                do                                                  
+                    {
+                        _theRandomNumber = _randomizer.Next(0,GetWordCount());
+                        if (_scriptureWordList[_theRandomNumber].GetStatus() == true) // checks for hidden state
+                            {   
+                                _theRandomNumber = _randomizer.Next(1,_scriptureWordList.Count);
+                            }
+                            else _isValid = true;
+                    }
+                while (!_isValid);
+                return _theRandomNumber;
+            }
+
+    }
